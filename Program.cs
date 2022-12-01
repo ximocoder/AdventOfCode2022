e6ce690c808f27ed488c1c3032804d4e697831ad
Ximo <Ximo@DESKTOP>
2022-12-01 13:24:19 +0100

    Day 1

--- Program.cs
+++ Program.cs
@@ -7,5 +7,44 @@ FindElfMostCalories();
 
 void FindElfMostCalories()
 {
-    string FileData = System.IO.File.ReadAllText("Day1Input.txt");
+    var FileData = System.IO.File.ReadAllLines("Day1Input.txt");
+
+    List<Elf> elfList = new List<Elf>();
+    
+    int numberoFelf = 1;
+    int totalCalories = 0;
+    foreach (var line in FileData)
+    {
+        
+
+        if (string.IsNullOrEmpty(line.Trim()))
+        {
+            Elf elf = new Elf()
+            {
+                numberOfelf = numberoFelf,
+                TotalCalories = totalCalories
+            };
+            elfList.Add(elf);
+            totalCalories = 0;
+            numberoFelf++;
+        }
+        else
+        {
+            totalCalories += int.Parse(line);
+        }
+    }
+
+    var result = elfList.Select(x => x.TotalCalories).Max();
+
+    var result2 = elfList.OrderByDescending(x => x.TotalCalories).Take(3).Sum(s => s.TotalCalories);
+
+
+    Console.WriteLine(result);
+
+}
+
+struct Elf
+{
+    public int numberOfelf;
+    public int TotalCalories;
 }
