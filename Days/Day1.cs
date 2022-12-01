92be88fcfd769a0c71042e890ae2c55c3b989e0e
Ximo <Ximo@DESKTOP>
2022-12-01 13:10:49 +0100

    Add project files.

---
+++ Days/Day1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022;
+
+public static internal class Day1
+{
+
+    static public void FindElfMostCalories()
+    {
+        string FileData = System.IO.File.ReadAllText(file);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, Aoc 2022!");
+
+FindElfMostCalories();
+
+
+void FindElfMostCalories()
+{
+    string FileData = System.IO.File.ReadAllText("Day1Input.txt");
+}
